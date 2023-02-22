cf4b4145a394feed74bc1d408ad7289de05ab463
Antoine MASSIP.ext <Antoine.MASSIP.ext@sodexo.com>
2023-02-22 15:20:36 +0100

    Example without DI

---
+++ DI.Examples/DAL/CommerceContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CommerceContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                             .SetBasePath(
+                             Directory.GetCurrentDirectory())
+                             .AddJsonFile("appsettings.json")
+                             .Build();
+
+            string connectionString = config.GetConnectionString("CommerceConnectionString");
+
+            builder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ DI.Examples/UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using BL;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UI.MVC.Models;
+
+namespace UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            bool isPreferredCustomer = this.User.IsInRole("PreferredCustomer");
+
+            var service = new ProductService();
+
+            var products = service.GetFeaturedProducts(isPreferredCustomer);
+
+            this.ViewData["Products"] = products;
+
+            return View();
+        }
+    }
+}
